62ff4eb302444725a72f43d80a6f351ae173c7e3
Edris89 <edriskeyam82@hotmail.com>
2019-04-01 17:23:05 +0200

    Build 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
      name='factocli',  
-     version='0.2.0',
+     version='0.2.1',
      scripts=['factocli'] ,
      author="Edris Keyam",
      author_email="edriskeyam82@hotmail.com",
