a474cc9319163627967604724fbbe39557b4498f
Edris89 <edriskeyam82@hotmail.com>
2019-04-08 21:33:45 +0200

    Build 0.2.4

--- setup.py
+++ setup.py
@@ -4,22 +4,22 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
-     name='factocli',  
-     version='0.2.3',
-     scripts=['factocli'] ,
-     author="Edris Keyam",
-     author_email="edriskeyam82@hotmail.com",
-     description="A Cli for managing a Factorio Headless Server",
-     long_description=long_description,
-   long_description_content_type="text/markdown",
-     url="https://github.com/Edris89/facto",
-     packages=setuptools.find_packages(),
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
-         "Operating System :: OS Independent",
-     ],
-     install_requires=["PyInquirer", "wget"],
+    name='factocli',  
+    version='0.2.4',
+    scripts=['factocli'] ,
+    author="Edris Keyam",
+    author_email="edriskeyam82@hotmail.com",
+    description="A Cli for managing a Factorio Headless Server",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Edris89/facto",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Linux",
+    ],
+    install_requires=["PyInquirer", "wget"],
  )
 
 
